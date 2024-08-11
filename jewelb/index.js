b58e67a4fa4637f27035e8cf0c08f3aa9e7bda6b
Navin Subramanian <assasin71003@gmail.com>
2024-08-11 10:17:52 +0530

    Vercel Host v2

--- jewelb/index.js
+++ jewelb/index.js
@@ -10,10 +10,11 @@ const app = express();
 const crypto = require('crypto');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-app.use(cors({
-    origin: '*'
-}));
-app.use(express.json());
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 const uploadDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
