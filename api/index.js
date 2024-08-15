42497e04a1a80e7df38a8f5b125d70de6a4ee901
Navin Subramanian <assasin71003@gmail.com>
2024-08-15 15:25:15 +0530

    VPS v2

--- api/index.js
+++ api/index.js
@@ -506,6 +506,12 @@ res.json(response[0])
         res.send(err)
     }
 })
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
